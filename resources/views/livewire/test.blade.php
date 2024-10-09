4898fe3000f7316f7affd47e2b0d699189f14b90
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-10-09 22:33:01 +0100

    💄 style (ui) : general ui for project resources

--- resources/views/livewire/test.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <input wire:model.live="search" />
-    <br>
-    <ul>
-        @foreach($users as $user)
-        <li>{{$user->name}}</li>
-        @endforeach
-    </ul>
-</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./resources/**/*.blade.php",
+      "./resources/**/*.blade.php",
   ],
   theme: {
     extend: {
-      font: {
-        sans: ['Segoe UI', 'sans-serif']
-      }
+        font: {
+            sans: ['Segoe UI', 'Roboto', 'sans-serif'],
+        }
     },
   },
   plugins: [],
 }
-
