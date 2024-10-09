d4d2b9f43f7281e3553fb986c88cfd7e0f154b2e
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-10-09 22:52:42 +0100

    ✨ feature (be) : proposal model, factory and seed

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -24,4 +24,9 @@ class Project extends Model
     {
         return $this->belongsTo(User::class, 'created_by');
     }
+
+    public function proposals()
+    {
+        return $this->hasMany(Proposal::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Project;
+use App\Models\Proposal;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,12 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()
-            ->count(200)
-            ->create();
+        User::factory()->count(200)->create();
 
-        User::query()
-            ->inRandomOrder()->limit(10)->get()
-            ->each(fn(User $user) => Project::factory()->create(['created_by' => $user->id]));
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(function (User $user) {
+                $project = Project::factory()->create(['created_by' => $user->id]);
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
     }
 }
